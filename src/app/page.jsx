f4c2a2a50fe451ac0251bc9d46225a980a703e44
fernandolim41 <pdvmobilidade@gmail.com>
2025-04-08 01:34:32 -0300

    minha atualização

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
 'use client';
-import React, { useState, useEffect } from 'react';
-import React, { useState } from 'react';
 
-
+import { useState, useEffect } from "react";
 
 function MainComponent() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
